c46ccd213b9a08b4a964a6e43f11ce5bb447572c
Justin <justin.cox56601@gamil.com>
2024-01-09 14:34:59 -0600

    converting master over to main

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CS3270</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+	<header class="header">
+		<div class="header-container">
+			<img src="./assets/swe-header.jpg" alt="software engineering header" class="header-image">
+			<p class="header-title">CS3270</p>
+			<p class="header-subtitle">Advanced Web Programming</p>
+			
+		</div>
+		<div class="nav-container">
+			<img src="./assets/dwarf_1.png" alt="" class="profile-img">
+			<nav class="nav">
+				<ul class="nav-list">
+					<li class="nav-item">Home</li>
+					<li class="nav-item">About</li>
+					<li class="nav-item">Assigments</li>
+					<li class="nav-item">Extra Credit</li>
+				</ul>
+			</nav>
+		</div>
+		
+	</header>
+	<div class="container">
+		<article class="article">
+		<h2>Assignment 1</h2>
+		<h3>Description</h3>
+		<p>
+			You are to create a 'Home' page. This page will include a header with either your name or 'Home' in it. 
+			A navigation bar with links to 'Home' and 'Assignments'. The links can be dead for now. As well as an article section where you answer the following questions.  
+			1. Why are you taking this class? 2.What do you want to take away from this class? Styling and creativity will be left up to you individually. You will also need
+			include a link on the page to the GitHub directory where all the source code for this assignment is located.
+		</p>
+		<h3 style="margin-top:1rem;">Acceptance Criteria</h3>
+		<ul style="margin-left:2rem;">
+			<li>Functioning Home Page</li>
+			<li>Header with either 'Home' or your name in it</li>
+			<li>Navigation with 'Home' and 'Assignements' links.  The links can be dead for now</li>
+			<li>Main Content section</li>
+			<li>'Why are you taking this class?' and 'What do you want to take away from this class?' in bold (h2 or h3 tag)</li>
+			<li>The answers to those two questions</li>
+			<li>A link to the GitHub repo with the source code</li>
+		</ul>
+		<h3 style="margin-top:1rem;">Delivery</h3>
+		<p>email justin.cox@bemidjistate.edu with the link to your working homepage. (https://cs.bemidjistate.edu/{yourIdNumber})</p>
+	</article>
+	</div>
+
+	
+
+	<script src="script.js"></script>
+</body>
+</html>
